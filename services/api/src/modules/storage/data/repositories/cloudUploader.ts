2e1a09fbcf8bed81d1979feae01c143ee93373dc
kevinand11 <kevinfizu@gmail.com>
2022-09-12 01:02:19 +0100

    feat: cloud uploader

---
+++ services/api/src/modules/storage/data/repositories/cloudUploader.ts
@@ -0,0 +1,40 @@
+import { IUploaderRepository } from '../../domain/irepositories/uploader'
+import { MediaInput } from '../models/media'
+import { environment } from '@utils/environment'
+import { Bucket, Storage } from '@google-cloud/storage'
+
+export class CloudUploaderRepository implements IUploaderRepository {
+	private bucket: Bucket
+
+	constructor () {
+		this.bucket = new Storage().bucket(`tutorstack${environment}.appspot.com`)
+	}
+
+	async delete (path: string) {
+		if (!path) return false
+		const file = this.bucket.file(path)
+		const exists = (await file.exists())[0]
+		if (exists) await file.delete()
+		return exists
+	}
+
+	async upload (path: string, media: MediaInput) {
+		const timestamp = Date.now()
+		media.name = media.name.toLowerCase()
+		path = `storage/${environment}/${path}/${timestamp}-${media.name}`
+
+		const file = this.bucket.file(path)
+
+		await file.save(media.data)
+
+		const link = file.publicUrl()
+
+		return {
+			name: media.name,
+			type: media.type,
+			size: media.size,
+			duration: media.duration,
+			path, timestamp, link
+		}
+	}
+}
--- services/api/src/modules/storage/index.ts
+++ services/api/src/modules/storage/index.ts
@@ -1,6 +1,8 @@
+import { CloudUploaderRepository } from './data/repositories/cloudUploader'
 import { LocalUploaderRepository } from './data/repositories/localUploader'
 import { StorageUseCase } from './domain/usecases/storage'
+import { isDev } from '@utils/environment'
 
-const uploaderRepository = new LocalUploaderRepository()
+const uploaderRepository = isDev ? new LocalUploaderRepository() : new CloudUploaderRepository()
 
 export const StorageUseCases = new StorageUseCase(uploaderRepository)
